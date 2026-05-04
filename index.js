6e451d94589d733b712d3c2b54847dfe04fb5414
thffhh <sb6201035@gmail.com>
2026-05-04 23:26:23 +0600

    Initialize Express and Socket.IO server
    Set up a basic Socket.IO server with Express.

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Socket server running 🚀");
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Server started");
+});
